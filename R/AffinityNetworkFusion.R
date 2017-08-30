1f3a99a964698c8cb6a8cc0e5ea2b783a542a990
BeautyOfWeb <tianlema.enjoy@gmail.com>
2017-08-30 18:12:47 -0400

    Update files

--- R/AffinityNetworkFusion.R
+++ R/AffinityNetworkFusion.R
@@ -12,20 +12,21 @@
 #' W = affinity_matrix(D, 5)
 #' S = kNN_graph(W, 5)
 kNN_graph = function(W, K) {
-  n = nrow(W)
-  # k-nearest neighbors does not include itself
-  # set similarity = 0 for n-K-1 farest neighbors 
-  if (n-K-1 < 1){
-    print("Error: K is too big: n-K-1 < 1. Assuming n > 2, set K = floor(n/2)")
-    K = floor(n/2)
-  }
-  idx = t(apply(W, 1, order)[1:(n-K-1),])
-  for (i in 1:n) {
-    W[i, idx[i,]] = 0
-  }
-  # row normalization --> transition matrix
-  S = W / rowSums(W)
-  return(S)
+    n = nrow(W)
+    # k-nearest neighbors does not include itself
+    # set similarity = 0 for n-K-1 farest neighbors
+    if (n - K - 1 < 1) {
+        print("Error: K is too big: n-K-1 < 1. Assuming n > 2, 
+          set K = floor(n/2)")
+        K = floor(n / 2)
+    }
+    idx = t(apply(W, 1, order)[1:(n - K - 1),])
+    for (i in 1:n) {
+        W[i, idx[i,]] = 0
+    }
+    # row normalization --> transition matrix
+    S = W / rowSums(W)
+    return(S)
 }
 
 #' Fuse affinity networks through one-step or two-step random walk
@@ -34,7 +35,7 @@ kNN_graph = function(W, K) {
 #' @param K the number of k nearest neighbors for function kNN_graph
 #' @param weight the weight for each view
 #' @param type 1: one-step random walk; 2: two-step random walk
-#' @param alpha used as weights for terms in type 2 (two-step random walk) 
+#' @param alpha used as weights for terms in type 2 (two-step random walk)
 #' @param verbose Boolean; if true, print some information
 #'
 #' @return a fused transition matrix (representing fused network)
@@ -45,105 +46,111 @@ kNN_graph = function(W, K) {
 #' D2 = matrix(runif(400),20)
 #' W2 = affinity_matrix(D1, 5)
 #' W = ANF(list(W1, W2), K=10)
-ANF = function(Wall, K = 20, weight=NULL, type=2, 
-               alpha = c(1,1,0,0,0,0,0,0), verbose=F) {
-  # sanity check Wall
-  if (!is.list(Wall)){
-    stop("Wall must be a list of matrices")
-  }
-  # n: number of views
-  n = length(Wall)
-  if (n==0) {
-    stop("Wall is empty")
-  }
-  if (n==1) {
-    if (verbose) {
-      print('length(Wall) = 1, return the only element.')
+ANF = function(Wall,
+               K = 20,
+               weight = NULL,
+               type = 2,
+               alpha = c(1, 1, 0, 0, 0, 0, 0, 0),
+               verbose = FALSE) {
+    # sanity check Wall
+    if (!is.list(Wall)) {
+        stop("Wall must be a list of matrices")
     }
-    return(Wall[[1]])
-  }
-  # <UnhandledAssertion> 
-  # 1. Elements of Wall are matrices of the same dimension
-  # 2. The rows and columns are aligned across all matrices
-  for (i in 1:n){
-    # row normalization: similarity matrix --> transition matrix 
-    Wall[[i]] = Wall[[i]] / rowSums(Wall[[i]])
-  }
-  
-  # N: number of points (objects)
-  N = nrow(Wall[[1]])
-  # normalize alpha
-  alpha = alpha / sum(alpha)
-  
-  # <?> should I ensure non-negative weights?
-  if(is.null(weight)) {
-    # assign uniform weight
-    weight = rep(1/n, n)
-  } 
-  if (length(weight) > n) {
-    print("WARNING: length(weight) > length(Wall). Discard the last Ws")
-    weight = weight[1:n]
-  }
-  if (length(weight) < n) {
-    print("WARNING: length(weight) < length(Wall). Assume the last Ws have 0 weight")
-    weight = c(weight, rep(0, n-length(weight)))
-  }
-  if (sum(weight!=0)==1){
-    print("only one view has a non-zero weight")
-    return(Wall[[which(weight!=0)]])
-  } else {
-    # normalize weight
-    if (sum(weight)!=0) {
-      weight = weight / sum(weight)
-    } else {
-      stop("sum(weight)==0")
+    # n: number of views
+    n = length(Wall)
+    if (n == 0) {
+        stop("Wall is empty")
     }
-  }
-  
-  # construct k-nearest-neighbor graph
-  # <?> Should it change during iterations? 
-  Sall = list()
-  for (j in 1:n){
-    Sall[[j]] = kNN_graph(Wall[[j]], K)
-  }
-  
-  if (type==1) {
-    W = matrix(0, N, N)
-    for (j in 1:j) {
-      # simplified version (does not include Wall[[j]])
-      W = W + weight[j] * Sall[[j]]
-    }
-  } else if (type==2) {
-    for (j in 1:n){
-      W = matrix(0,N,N)
-      S = matrix(0,N,N)
-      sumWeight=sum(weight) - weight[j]
-      if (sumWeight == 0){
-        print("WARNING: sum(weight) - weight[j] == 0. 
-              The result is probably unreliable.")
-        sumWeight = sum(weight)
-      }
-      for (k in 1:n) {
-        if (k!=j) {
-          # complementary view
-          W = W + (weight[k] / sumWeight) * Wall[[k]]
-          S = S + (weight[k] / sumWeight) * Sall[[k]]
+    if (n == 1) {
+        if (verbose) {
+            print('length(Wall) = 1, return the only element.')
         }
-      }
-      
-      Wall[[j]] = alpha[1] * Sall[[j]] %*% S + alpha[2] * S %*% Sall[[j]] + 
-        alpha[3] * Sall[[j]] %*% W + alpha[4] * W %*% Sall[[j]] + 
-        alpha[5] * Wall[[j]] %*% S  + alpha[6] * S %*% Wall[[j]] +
-        alpha[7] * Wall[[j]] %*% W + alpha[8] * W %*% Wall[[j]]
+        return(Wall[[1]])
     }
-  } else {
-    stop("type other than 1 or 2 is undefined")
-  }
-  W = matrix(0,N,N)
-  for (j in 1:n) {
-    W = W + weight[j] * Wall[[j]]
-  }
-  rownames(W) = rownames(Wall[[1]])
-  colnames(W) = colnames(Wall[[1]])
-  return(W)
+    # <UnhandledAssertion>
+    # 1. Elements of Wall are matrices of the same dimension
+    # 2. The rows and columns are aligned across all matrices
+    for (i in 1:n) {
+        # row normalization: similarity matrix --> transition matrix
+        Wall[[i]] = Wall[[i]] / rowSums(Wall[[i]])
+    }
+    
+    # N: number of points (objects)
+    N = nrow(Wall[[1]])
+    # normalize alpha
+    alpha = alpha / sum(alpha)
+    
+    # <?> should I ensure non-negative weights?
+    if (is.null(weight)) {
+        # assign uniform weight
+        weight = rep(1 / n, n)
+    }
+    if (length(weight) > n) {
+        print("WARNING: length(weight) > length(Wall).
+          Discard the last Ws")
+        weight = weight[1:n]
+    }
+    if (length(weight) < n) {
+        print("WARNING: length(weight) < length(Wall).
+          Assume the last Ws have 0 weight")
+        weight = c(weight, rep(0, n - length(weight)))
+    }
+    if (sum(weight != 0) == 1) {
+        print("only one view has a non-zero weight")
+        return(Wall[[which(weight != 0)]])
+    } else {
+        # normalize weight
+        if (sum(weight) != 0) {
+            weight = weight / sum(weight)
+        } else {
+            stop("sum(weight)==0")
+        }
+    }
+    
+    # construct k-nearest-neighbor graph
+    # <?> Should it change during iterations?
+    Sall = list()
+    for (j in 1:n) {
+        Sall[[j]] = kNN_graph(Wall[[j]], K)
+    }
+    
+    if (type == 1) {
+        W = matrix(0, N, N)
+        for (j in 1:j) {
+            # simplified version (does not include Wall[[j]])
+            W = W + weight[j] * Sall[[j]]
+        }
+    } else if (type == 2) {
+        for (j in 1:n) {
+            W = matrix(0, N, N)
+            S = matrix(0, N, N)
+            sumWeight = sum(weight) - weight[j]
+            if (sumWeight == 0) {
+                print("WARNING: sum(weight) - weight[j] == 0.
+              The result is probably unreliable.")
+                sumWeight = sum(weight)
+            }
+            for (k in 1:n) {
+                if (k != j) {
+                    # complementary view
+                    W = W + (weight[k] / sumWeight) * Wall[[k]]
+                    S = S + (weight[k] / sumWeight) * Sall[[k]]
+                }
+            }
+            
+            Wall[[j]] = alpha[1] * Sall[[j]] %*% S + alpha[2] * S %*% Sall[[j]] +
+                alpha[3] * Sall[[j]] %*% W + alpha[4] * W %*% Sall[[j]] +
+                alpha[5] * Wall[[j]] %*% S  + alpha[6] * S %*% Wall[[j]] +
+                alpha[7] * Wall[[j]] %*% W + alpha[8] * W %*% Wall[[j]]
+        }
+    } else {
+        stop("type other than 1 or 2 is undefined")
+    }
+    W = matrix(0, N, N)
+    for (j in 1:n) {
+        W = W + weight[j] * Wall[[j]]
+    }
+    rownames(W) = rownames(Wall[[1]])
+    colnames(W) = colnames(Wall[[1]])
+    return(W)
 }
--- R/SpectralClustering.R
+++ R/SpectralClustering.R
@@ -1,4 +1,5 @@
-#' Generate a symmetric affinity matrix based on distance matrix using 'local' Gaussian kernel
+#' Generate a symmetric affinity matrix based on distance matrix 
+#' using 'local' Gaussian kernel
 #'
 #' @param D distance matrix
 #' @param k the number of k-nearest neighbors
@@ -13,19 +14,19 @@
 #' A = affinity_matrix(D, 5)
 affinity_matrix <- function(D, k, alpha=1/6, beta=1/6)
 {
-  n = nrow(D)
-  # make it symmetric in case it is not
-  D = (D + t(D)) / 2
-  diag(D) = 0
-  D[D<0] = 0
-  finiteMean = function(x){
-    return(mean(x[is.finite(x)]))
-  }
-  # <Tricky> k nearest neighbors should exclude itself: 1:k+1
-  # <?> here we divide by k+1, maybe k is better
-  d = apply(apply(D, 2, sort)[1:k+1,], 2, finiteMean)
-  sigma = alpha * (outer(d, d, '+')) + beta * D + .Machine$double.eps
-  return(dnorm(D, 0, sigma))
+    n = nrow(D)
+    # make it symmetric in case it is not
+    D = (D + t(D)) / 2
+    diag(D) = 0
+    D[D<0] = 0
+    finiteMean = function(x){
+        return(mean(x[is.finite(x)]))
+    }
+    # <Tricky> k nearest neighbors should exclude itself: 1:k+1
+    # <?> here we divide by k+1, maybe k is better
+    d = apply(apply(D, 2, sort)[1:k+1,], 2, finiteMean)
+    sigma = alpha * (outer(d, d, '+')) + beta * D + .Machine$double.eps
+    return(stats::dnorm(D, 0, sigma))
 }
 #' Finding optimal discrete solutions for spectral clustering
 #' 
@@ -45,41 +46,41 @@ affinity_matrix <- function(D, k, alpha=1/6, beta=1/6)
 #' e = eigen(NL)
 #' Y = Re(e$vectors[,-1:-17])
 #' X = pod(Y)
-pod = function(Y, verbose=F) {
-  if(!is.matrix(Y)) {
-    stop("Y should be a matrix")
-  } else {
-    # normalize in case the input is not
-    Y = Y / sqrt(rowSums(Y^2))
-  }
-  N = nrow(Y)
-  K = ncol(Y)
-  if ( N<K) {
-    stop("Y should have more rows than columns")
-  }
-  R = matrix(0, K, K)
-  # chose any row of Y, here the middle row
-  R[,1] = Y[round(N/2),]
-  c = rep(0, N)
-  for (i in 2:K) {
-    c = c + abs(Y %*% R[,i-1])
-    R[,i] = Y[which.min(c),]
-  }
-  e = 0
-  error = 1
-  while (error > .Machine$double.eps) {
-    idx = apply(Y %*% R, 1, which.max)
-    X = matrix(0, N, K)
-    X[cbind(1:N, idx)] = 1
-    s = svd(t(X)%*%Y)
-    R = s$v %*% t(s$u)
-    error = abs(sum(s$d)-e)
-    e = sum(s$d)
-  } 
-  if(verbose) {
-    print(paste("e =", e))
-  }
-  return(X)
+pod = function(Y, verbose=FALSE) {
+    if(!is.matrix(Y)) {
+        stop("Y should be a matrix")
+    } else {
+        # normalize in case the input is not
+        Y = Y / sqrt(rowSums(Y^2))
+    }
+    N = nrow(Y)
+    K = ncol(Y)
+    if ( N<K) {
+        stop("Y should have more rows than columns")
+    }
+    R = matrix(0, K, K)
+    # chose any row of Y, here the middle row
+    R[,1] = Y[round(N/2),]
+    c = rep(0, N)
+    for (i in 2:K) {
+        c = c + abs(Y %*% R[,i-1])
+        R[,i] = Y[which.min(c),]
+    }
+    e = 0
+    error = 1
+    while (error > .Machine$double.eps) {
+        idx = apply(Y %*% R, 1, which.max)
+        X = matrix(0, N, K)
+        X[cbind(1:N, idx)] = 1
+        s = svd(t(X)%*%Y)
+        R = s$v %*% t(s$u)
+        error = abs(sum(s$d)-e)
+        e = sum(s$d)
+    } 
+    if(verbose) {
+        print(paste("e =", e))
+    }
+    return(X)
 }
 #' spectral_clustering
 #'
@@ -95,31 +96,31 @@ pod = function(Y, verbose=F) {
 #' D = matrix(runif(400),20)
 #' A = affinity_matrix(D, 5)
 #' labels = spectral_clustering(A, 2)
-spectral_clustering <- function(A, k, type=2, verbose=F)
+spectral_clustering <- function(A, k, type=2, verbose=FALSE)
 {
-  n = nrow(A)
-  d = rowSums(A)
-  d[d == 0] = .Machine$double.eps
-  L = diag(d) - A
-  if(type==1) {
-    NL = L
-  } else if(type==2) {
-    NL = diag(1/d) %*% L
-  } else if(type == 3) {
-    NL = diag(1/sqrt(d)) %*% L %*% diag(1/sqrt(d))
-  } else {
-    stop("type should be 1, 2, or 3")
-  }
-  e = eigen(NL)
-  if(verbose) {
-    print(paste("the k+3 smallest eigen values:", e$values[-1:-(n-k-3)]))
-  }
-  Y = Re(e$vectors[,-1:-(n-k)])
-  if (type == 3) {
-    Y = Y / sqrt(rowSums(Y ^ 2))
-  }
-  labels = apply(pod(Y),1,which.max)
-  return(labels)
+    n = nrow(A)
+    d = rowSums(A)
+    d[d == 0] = .Machine$double.eps
+    L = diag(d) - A
+    if(type==1) {
+        NL = L
+    } else if(type==2) {
+        NL = diag(1/d) %*% L
+    } else if(type == 3) {
+        NL = diag(1/sqrt(d)) %*% L %*% diag(1/sqrt(d))
+    } else {
+        stop("type should be 1, 2, or 3")
+    }
+    e = eigen(NL)
+    if(verbose) {
+        print(paste("the k+3 smallest eigen values:", e$values[-1:-(n-k-3)]))
+    }
+    Y = Re(e$vectors[,-1:-(n-k)])
+    if (type == 3) {
+        Y = Y / sqrt(rowSums(Y ^ 2))
+    }
+    labels = apply(pod(Y),1,which.max)
+    return(labels)
 }
 
 #' Evaluate cluster result 
@@ -138,45 +139,58 @@ spectral_clustering <- function(A, k, type=2, verbose=F)
 #'
 #' @return a vector of Normalized Mutual Informatin (NMI), Adjusted Rand Index (ARI),
 #' and -log10(p) of log rank test of survival distributions of patient clusters
+#' @import igraph MASS Biobase
 #' @export
+#' @examples 
+#' true.class = rep(c(1,2),each=100)
+#' feature.mat1 = MASS::mvrnorm(100, rep(0, 20), diag(runif(20,0.2,2)))
+#' feature.mat2 = MASS::mvrnorm(100, rep(0.5, 20), diag(runif(20,0.2,2)))
+#' feature1 = rbind(feature.mat1, feature.mat2)
+#' d = dist(feature1)
+#' d = as.matrix(d)
+#' A = affinity_matrix(d, 10)
+#' res = eval_clu(true_class=true.class, w=A)
 eval_clu = function(true_class, w= NULL, d = NULL, 
                     k = 10, num_clu = NULL, surv=NULL, 
-                    type_L=2, verbose=T) {
-  if(is.null(w) & is.null(d)){
-    stop("Either distance or affinity matrix must be provided")
-  }
-  if(is.null(w) & !is.null(d)) {
-    w = affinity_matrix(d, min(k, nrow(d)-1))
-  }
-  # <InputRequirement> 
-  if(!all(rownames(w) %in% names(true_class))) {
-    stop("some rownames(w) is unknown to true_class")
-  }
-  true_class = as.factor(true_class[rownames(w)])
-  if(is.null(num_clu)) {
-    num_clu = length(unique(true_class))
-  }
-  labels = spectral_clustering(w, num_clu, type = type_L)
-  nmi = compare(true_class, labels, method='nmi')
-  adjusted.rand = compare(true_class, labels, method='adjusted.rand')
-  
-  if(!is.null(surv) & 
-     length(intersect(rownames(w), rownames(surv))) > nrow(w)/2) {
-    surv = surv[rownames(w),]
-    f = Surv(surv$time, !surv$censored)
-    fit = survdiff(f~labels)
-    pval = pchisq(fit$chisq, df=length(fit$n)-1, lower.tail = F)
-  } else {
-    pval = NA
-  }
-  if(verbose) {
-    print(paste('nmi:', nmi))
-    print(paste('adjusted.rand:', adjusted.rand))
-    print(table(true_class, labels))
-    print(paste('survdiff p value:', pval))
-  }
-  
-  clu.res = c(nmi, adjusted.rand, -log10(pval))
-  names(clu.res) = c("NMI", "ARI", "-log10(p)")
-  return(list(w=w, clu.res=clu.res, labels=labels))
+                    type_L=2, verbose=TRUE) {
+    if(is.null(w) & is.null(d)){
+        stop("Either distance or affinity matrix must be provided")
+    }
+    if(is.null(w) & !is.null(d)) {
+        w = affinity_matrix(d, min(k, nrow(d)-1))
+    }
+    # <InputRequirement> 
+    if (is.null(names(true_class))) {
+        names(true_class) = rownames(w)
+    }
+    if(!all(rownames(w) %in% names(true_class))) {
+        stop("some rownames(w) is unknown to true_class")
+    }
+    true_class = as.factor(true_class[rownames(w)])
+    if(is.null(num_clu)) {
+        num_clu = length(unique(true_class))
+    }
+    labels = spectral_clustering(w, num_clu, type = type_L)
+    nmi = igraph::compare(true_class, labels, method='nmi')
+    adjusted.rand = igraph::compare(true_class, labels, method='adjusted.rand')
+    
+    if(!is.null(surv) & 
+       length(intersect(rownames(w), rownames(surv))) > nrow(w)/2) {
+        surv = surv[rownames(w),]
+        f = survival::Surv(surv$time, !surv$censored)
+        fit = survival::survdiff(f~labels)
+        pval = stats::pchisq(fit$chisq, df=length(fit$n)-1, lower.tail = FALSE)
+    } else {
+        pval = NA
+    }
+    if(verbose) {
+        print(paste('nmi:', nmi))
+        print(paste('adjusted.rand:', adjusted.rand))
+        print(table(true_class, labels))
+        print(paste('survdiff p value:', pval))
+    }
+    
+    clu.res = c(nmi, adjusted.rand, -log10(pval))
+    names(clu.res) = c("NMI", "ARI", "-log10(p)")
+    return(list(w=w, clu.res=clu.res, labels=labels))
 }
